6b41b788223c5c416a601140938cf35e90df87d0
Nate Schultz <nate.schultz@outlook.com>
2015-05-15 00:11:12 -0400

    set up javascript

---
+++ scripts/canvas.js
@@ -0,0 +1,24 @@
+﻿
+var game_started = false;
+
+function frame() {
+
+}
+
+function move() {
+
+}
+
+//$("#canvas").onkeypress = function () {
+//    window.alert("Key Pressed");
+//};
+
+function keyPressed(e) {
+    
+    //pressed space - start game
+    if (event.keyCode == 32 && game_started == false) {
+        $("#spaceToPlay")[0].style.display = "none";
+        game_started = true;
+        frame()
+    }
+}
