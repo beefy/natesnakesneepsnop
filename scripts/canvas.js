0c7cebfd79cf49d74276441e4f22d64e0e2b93e5
Nate Schultz <nate.schultz@outlook.com>
2015-05-15 00:35:37 -0400

    started making snake grid

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -2,11 +2,12 @@
 var game_started = false;
 
 function frame() {
-
+    setTimeout(frame, 100);
+    move_snake();
 }
 
 function move() {
-
+    $("#snake")[0].style.marginleft = $("#snake")[0].style.marginleft + 1;
 }
 
 //$("#canvas").onkeypress = function () {
@@ -18,6 +19,7 @@ function keyPressed(e) {
     //pressed space - start game
     if (event.keyCode == 32 && game_started == false) {
         $("#spaceToPlay")[0].style.display = "none";
+        $("#snake")[0].style.display = "block";
         game_started = true;
         frame()
     }
