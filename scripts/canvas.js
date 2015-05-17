76b1439d4f48874235dca269ac4836d6cc570bc7
Nate Schultz <nate.schultz@outlook.com>
2015-05-17 01:21:55 -0400

    got movement to work

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -1,26 +1,97 @@
 ﻿
 var game_started = false;
+var active_snake_parts = [   [1,2], [1,3], [1,4], [2,4]  ];
+var move_dir = "left";
 
 function frame() {
-    setTimeout(frame, 100);
+    check_death();
+    check_grow();
     move_snake();
+    setTimeout(frame, 500);
 }
 
-function move() {
-    $("#snake")[0].style.marginleft = $("#snake")[0].style.marginleft + 1;
+function move_snake() {
+
+    //remove old display
+    for (var i = 0; i < active_snake_parts.length; i++) {
+        $("table tr:nth-child(" + active_snake_parts[i][1] + ") td:nth-child(" + active_snake_parts[i][0] + ")").css("background-color", "black");
+    }
+
+    //change which part is active
+    for (var i = 0; i < active_snake_parts.length; i++) {
+            if(i+1 < active_snake_parts.length) {
+                active_snake_parts[i][0] = active_snake_parts[i + 1][0];
+                active_snake_parts[i][1] = active_snake_parts[i + 1][1];
+            } else {
+                if (move_dir == "left") {
+                    active_snake_parts[i][0]++;
+                } else if(move_dir == "right") {
+                    active_snake_parts[i][0]--;
+                } else if(move_dir == "down") {
+                    active_snake_parts[i][1]++;
+                } else if(move_dir =="up") {
+                    active_snake_parts[i][1]--;
+                }
+            }
+
+    }
+
+    //display
+    for (var i = 0; i < active_snake_parts.length; i++) {
+
+        $("#snake tr:nth-child(" + active_snake_parts[i][1] + ") td:nth-child(" + active_snake_parts[i][0] + ")").css("background-color", "white");
+    }
 }
 
-//$("#canvas").onkeypress = function () {
-//    window.alert("Key Pressed");
-//};
+function check_death() {
 
-function keyPressed(e) {
+}
+
+function check_grow() {
+
+}
+
+function keyDown(e) {
     
+    //prevent scrolling
+    if ([32, 37, 38, 39, 40].indexOf(event.keyCode) > -1) {
+        event.preventDefault();
+    }
+
     //pressed space - start game
     if (event.keyCode == 32 && game_started == false) {
         $("#spaceToPlay")[0].style.display = "none";
         $("#snake")[0].style.display = "block";
         game_started = true;
         frame()
+    }
+
+    //pressed right or 'd'
+    if(game_started && move_dir != "right" && 
+        (event.keyCode == 39 || event.keyCode == 68)) {
+        move_dir = "left";
+    }
+
+    //pressed left or 'a'
+    if(game_started && move_dir != "left" && 
+        (event.keyCode == 37 || event.keyCode == 65)) {
+        move_dir = "right";
+    }
+
+    //pressed up or 'w'
+    if(game_started && move_dir != "down" && 
+        (event.keyCode == 38 || event.keyCode == 87)) {
+        move_dir = "up";
+    }
+
+    //pressed down or 's'
+    if(game_started && move_dir != "up" && 
+        (event.keyCode == 40 || event.keyCode == 83)) {
+        move_dir = "down";
+    }
+
+    //pressed 'p'
+    if(game_started && event.keyCode == 112) {
+        move_dir = "paused";
     }
 }
